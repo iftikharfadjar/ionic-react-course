68b5a16b40ddb0c08043b8d2baea90571531559c
fadjar iftikhar <fadjar.iftikhar@gmail.com>
2020-07-25 15:29:13 +0700

    Add sample IonButton and IonToast Component

--- ionic-react-course/src/App.js
+++ ionic-react-course/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , {useState} from 'react'
+import { IonButton, IonToast } from  '@ionic/react'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [showToast, setShowToast] = useState(false)
+    const handleclick = () => {
+        setShowToast(!showToast)
+    }
+
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <IonButton onClick={handleclick}>Click Me</IonButton>
+            <IonToast isOpen={showToast} message="Hi World" />
+        </div>
+    )
 }
 
-export default App;
+export default App
