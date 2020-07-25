c21f1076fbfc6fe095df33be1b75cf8012783201
FadjarIftikhar <fadjar.iftikhar@gmail.com>
2020-07-25 17:37:03 +0700

    add axios and get name from fake json data sample

--- ionic-react-course/src/App.js
+++ ionic-react-course/src/App.js
@@ -1,20 +1,52 @@
-import React , {useState} from 'react'
-import { IonButton, IonToast } from  '@ionic/react'
+import React from 'react'
+import { IonButton, IonToast, IonLabel } from  '@ionic/react'
+import axios from 'axios'
+import Users from './Users.js'
 
 
-function App() {
-    const [showToast, setShowToast] = useState(false)
-    const handleclick = () => {
-        setShowToast(!showToast)
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            users:[],
+            showToast: false
+          }
+
+        // This binding is necessary to make `this` work in the callback
+        this.handleclick = this.handleclick.bind(this);
+      }
+
+    //   Lifecycle method
+    async componentDidMount(){
+        const res = await axios.get('https://jsonplaceholder.typicode.com/users')
+        this.setState({ users: res.data });
+        console.log(this.state)
     }
-
-    return (
-        <div>
-            <h1>Hello World</h1>
-            <IonButton onClick={handleclick}>Click Me</IonButton>
-            <IonToast isOpen={showToast} message="Hi World" />
-        </div>
-    )
+    
+      handleclick() {
+        this.setState(state => ({...state, 
+            showToast: !state.showToast
+        }));
+      }
+ 
+      render(){
+        return (
+            <div>
+               
+                <h1>Hello World</h1>
+                <IonButton onClick={this.handleclick}>Click Me</IonButton>
+                <IonToast isOpen={this.state.showToast} message="Hi World" />
+                {this.state.users.map(user => (
+                    <div key={user.name}>
+                        <IonLabel>{user.name}</IonLabel>
+                    </div>
+                )) }
+         
+            </div>
+        )
+    }
 }
 
 export default App
---
+++ ionic-react-course/src/Users.js
@@ -0,0 +1,95 @@
+import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.css';
+import '@ionic/core/css/core.css';
+import '@ionic/core/css/ionic.bundle.css';
+
+
+
+import {
+  IonContent,
+  IonRow,
+  IonGrid,
+  IonCol,
+} from '@ionic/react';
+
+
+//=================================================================
+//          STATELESS FUNCTIONAL COMPONENT
+//=================================================================
+const Users = ({users}) => {
+
+      return (
+      <>
+         <IonContent>
+          <IonGrid Fixed>
+            <IonRow>
+                {users.map(user => (
+                  <IonCol  size-lg="3" size-md="4" size-sm="6" size-xs="12" key={user.id}>
+                      <h1>{user.login}</h1>
+                  </IonCol>
+                ))}
+
+            </IonRow>
+          </IonGrid>
+       </IonContent>
+      </>
+     );
+   
+  
+}
+
+//=================================================================
+//          CLASS COMPONENT
+//=================================================================
+// class Users extends Component {
+// //data sample
+// //   constructor(){
+// //     super();
+// //     this.state = {
+// //       users : props.users
+// //       users: [
+// //         {
+// //           id: '1',
+// //           login: 'mojombo',
+// //           avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+// //           html_url:"https://github.com/mojombo"
+// //         },
+// //         {
+// //           id: '2',
+// //           login: "defunkt",
+// //           avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
+// //           html_url: "https://github.com/defunkt",
+// //         },
+// //         {
+// //           login: "pjhyett",
+// //           id: '3',
+// //           avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
+// //           html_url: "https://github.com/pjhyett",
+// //         }
+// //       ]
+// //     }
+// //   }
+  
+//   render(){
+//     //for re-variable of this.state
+//     return (
+//     <>
+//        <IonContent>
+//         <IonGrid Fixed>
+//           <IonRow>
+//               {this.props.users.map(user => (
+//                 <IonCol  size-lg="3" size-md="4" size-sm="6" size-xs="12" key={user.id}>
+//                     <UserItem user={user}/>
+//                 </IonCol>
+//               ))}
+   
+//           </IonRow>
+//         </IonGrid>
+//      </IonContent>
+//     </>
+//    );
+//   }
+// }
+
+
+export default Users
